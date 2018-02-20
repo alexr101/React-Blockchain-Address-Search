b691c52521b8457c4524b00bbf6d730b62568e9c
alexr101 <alexonezero@outlook.com>
2018-02-20 12:32:28 -0500

    separate babel backend and clientside

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,18 +4,21 @@ import gulp from 'gulp';
 import inject from 'gulp-inject';
 import babel from 'gulp-babel';
 import sourcemaps from 'gulp-sourcemaps';
+import source from 'vinyl-source-stream';
 import concat from 'gulp-concat';
-import browserify from 'gulp-browserify'
+import browserify from 'browserify';
 import del from 'del';
 import runSequence from 'run-sequence';
 import debug from 'gulp-debug';
+import babelify from 'babelify';
+import rename from 'gulp-rename';
 
-gulp.task('inject', ['build'], () => {
+gulp.task('inject', () => {
 	let paths = [
 		'./public/build/app/app.js',
 		'./public/build/css/**/*.css',
 	]
-	
+
 	return gulp.src('./public/build/index.html')
 		.pipe(debug())
 		.pipe(inject(gulp.src(paths, { read: true }),
@@ -27,41 +30,46 @@ gulp.task('inject', ['build'], () => {
 		.pipe(gulp.dest('./public/build'));
 });
 
-gulp.task('babel-transpile', () => {
-	return gulp.task('default', () =>
-		gulp.src('./public/src/app/**/*.js')
-			.pipe(sourcemaps.init())
-			.pipe(babel({
-				presets: ['env', 'react']
-			}))
-			.pipe(browserify({
-				transform: ['babelify'],
-			}))
-			.pipe(gulp.dest('./public/build/app'))
-	);
+gulp.task('babel', () => {
+	return gulp.src('./public/src/app/app.js')
+		.pipe(babel({
+			presets: ['es2015', 'react']
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(gulp.dest('./public/build/app'))
+
+});
+
+gulp.task('client', () => {
+	return browserify('public/build/app/app.js')
+		.transform(babelify, { presets: ["es2015", "react"] })
+		.bundle()
+		.pipe(source('./public/src/app/app.js'))
+		.pipe(rename({ dirname: './' }))
+		.pipe(gulp.dest('public/build/app'))
+
 });
 
 gulp.task('build', () => {
 	let filesToMove = [
 		'./public/src/**/*.*',
 	];
-
 	return gulp.src(filesToMove)
 		.pipe(gulp.dest('./public/build/'));
 })
 
 gulp.task('del-build', () => {
 	return del([
-    './public/build',
-  ]);
+		'./public/build',
+	]);
 })
 
 gulp.task('default', (cb) => {
 	runSequence(
 		'del-build',
-		'babel-transpile',
 		'build',
-		'inject',
-		cb
+		'babel',
+		'client',
+		'inject'
 	)
 })
