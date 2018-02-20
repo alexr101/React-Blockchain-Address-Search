3fd192071105a7ecf8959b1b0eefb0208a2ed31d
alexr101 <alex_r_101@hotmail.com>
2018-02-19 22:13:49 -0500

    react app skeleton small fixes, package.json updates

--- public/src/app/app.js
+++ public/src/app/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AddressSearch from './components/address/addressSearch';
 
 ReactDOM.render(
 	<div>My App</div>,
---
+++ public/src/app/components/address/addressSearch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import axios from 'axios';
+
+// export default class AddressSearch extends Component {
+// 	constructor() {
+// 		super();
+// 		this.state = { query: '' }
+// 	}
+
+// 	getData() {
+// 		const q
+// 	}
+// }
